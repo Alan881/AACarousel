57b1d168bb4950efa72e5839310287cbbb345eee
Alan.Huang <Alan.Huang@infotimes.com.tw>
2017-06-11 16:55:47 +0800

    init project

---
+++ AACarousel/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AACarousel
+//
+//  Created by Alan on 2017/6/11.
+//  Copyright © 2017年 Alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
