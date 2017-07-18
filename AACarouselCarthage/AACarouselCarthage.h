478b0ed417d0082c8a46076f829ccbaa2c53d506
Alan.Huang <Alan.Huang@infotimes.com.tw>
2017-07-18 22:50:42 +0800

    Add Carthage support

---
+++ AACarouselCarthage/AACarouselCarthage.h
@@ -0,0 +1,19 @@
+//
+//  AACarouselCarthage.h
+//  AACarouselCarthage
+//
+//  Created by Alan on 2017/7/18.
+//  Copyright © 2017年 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AACarouselCarthage.
+FOUNDATION_EXPORT double AACarouselCarthageVersionNumber;
+
+//! Project version string for AACarouselCarthage.
+FOUNDATION_EXPORT const unsigned char AACarouselCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AACarouselCarthage/PublicHeader.h>
+
+
