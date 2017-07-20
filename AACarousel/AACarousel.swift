95a15a9388146a2dceff62f2e8cc0d7780986b96
Alan.Huang <Alan.Huang@infotimes.com.tw>
2017-07-20 14:44:51 +0800

    Change project to public in framework

--- AACarousel/AACarousel.swift
+++ AACarousel/AACarousel.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-@objc protocol AACarouselDelegate {
+@objc public protocol AACarouselDelegate {
    @objc optional func didSelectCarouselView(_ view:AACarousel, _ index:Int)
    @objc optional func callBackFirstDisplayView(_ imageView:UIImageView, _ url:[String], _ index:Int)
    func downloadImages(_ url:String, _ index:Int)
@@ -16,7 +16,7 @@ import UIKit
 
 let needDownload = "http"
 
-class AACarousel: UIView,UIScrollViewDelegate {
+public class AACarousel: UIView,UIScrollViewDelegate {
     
     var delegate:AACarouselDelegate?
     var images = [UIImage]()
@@ -58,7 +58,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
         
     }
     
-    override func layoutSubviews() {
+    override public func layoutSubviews() {
         super.layoutSubviews()
         
         setScrollViewFrame()
@@ -71,7 +71,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
     }
     
     //MARK:- Interface Builder(Xib,StoryBoard)
-    override func awakeFromNib() {
+    override public func awakeFromNib() {
         super.awakeFromNib()
         
         initWithScrollView()
@@ -83,7 +83,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
         setNeedsDisplay()
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         //fatalError("init(coder:) has not been implemented")
     }
@@ -254,7 +254,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
     }
     
     //MARK:- set subviews layout method
-    func setCarouselLayout(displayStyle:Int, pageIndicatorPositon:Int, pageIndicatorColor:UIColor?, describedTitleColor:UIColor?, layerColor:UIColor?) {
+    public func setCarouselLayout(displayStyle:Int, pageIndicatorPositon:Int, pageIndicatorColor:UIColor?, describedTitleColor:UIColor?, layerColor:UIColor?) {
         
         carouselMode = displayModel.init(rawValue: displayStyle) ?? .full
         indicatorPosition = pageControlPosition.init(rawValue: pageIndicatorPositon) ?? .bottom
@@ -265,7 +265,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
     }
     
     //MARK:- set subviews show method
-    func setCarouselOpaque(layer:Bool, describedTitle:Bool, pageIndicator:Bool) {
+    public func setCarouselOpaque(layer:Bool, describedTitle:Bool, pageIndicator:Bool) {
     
         layerView.isHidden = layer
         describedLabel.isHidden = describedTitle
@@ -274,7 +274,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
     
    
     //MARK:- set data method
-    func setCarouselData(paths:[String],describedTitle:[String],isAutoScroll:Bool,timer:Double?,defaultImage:String?) {
+    public func setCarouselData(paths:[String],describedTitle:[String],isAutoScroll:Bool,timer:Double?,defaultImage:String?) {
         
         if paths.count == 0 {
             return
@@ -456,7 +456,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
     
    
     //MARK:- UIScrollViewDelegate
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
         
         if images.count == 0  {
             return
@@ -477,7 +477,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
         
     }
     
-    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         
         switch carouselMode {
         case .full:
@@ -490,7 +490,7 @@ class AACarousel: UIView,UIScrollViewDelegate {
     }
     
     
-    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
        
         startAutoScroll()
         
@@ -544,12 +544,12 @@ class AACarousel: UIView,UIScrollViewDelegate {
     
     
     //MARK:- public control method
-    func startScrollImageView() {
+    public func startScrollImageView() {
         
         startAutoScroll()
     }
     
-    func stopScrollImageView() {
+    public func stopScrollImageView() {
         
         stopAutoScroll()
     }
